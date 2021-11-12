3082943d296ccfdf57fb6b3c71e6edd91b819877
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-11-12 09:48:48 +0900

    wip

--- components/information/myComponent.js
+++ components/information/myComponent.js
@@ -3,7 +3,7 @@
 import Image from "next/image";
 export default function MyComponent() {
   return (
-    <div className="flex justify-center flex-wrap mx-2 my-4">
+    <div className="flex justify-center flex-wrap mx-2 my-12">
       <Image
         className="w-full lg:w-1/2 fadeUp"
         width={400}
@@ -19,7 +19,7 @@ export default function MyComponent() {
             <p>VRChat楽しい</p>
           </div>
         </div>
-        <div className="h-2/4 fadeUp delay-time02 flex items-center py-2 justify-center">
+        <div className="h-2/4 fadeUp delay-time04 flex items-center py-2 justify-center">
           <div className="w-1/12 ">
             <span class="material-icons arrow-rotate">arrow_back</span>
           </div>
@@ -27,12 +27,12 @@ export default function MyComponent() {
             <p>キャラメルポップコーン美味しい</p>
           </div>
         </div>
-        <div className="h-2/4 fadeUp delay-time02 flex items-center py-2 justify-center">
+        <div className="h-1/4 fadeUp delay-time06 flex items-center py-2 justify-center">
           <div className="w-1/12 ">
             <span class="material-icons arrow-rotate">arrow_back</span>
           </div>
           <div className="w-11/12">
-            <p>VRChat楽しい</p>
+            <p>QOLが大事</p>
           </div>
         </div>
       </div>
