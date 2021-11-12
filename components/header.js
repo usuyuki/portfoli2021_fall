fdc71279b0de8e50651f808d3e7ac1005ee5ae0f
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-11-12 09:46:12 +0900

    feat ヘッダーのCSSアニメーション

--- components/header.js
+++ components/header.js
@@ -3,8 +3,11 @@ import Link from "next/link";
 export default function Header(props) {
   return (
     <div>
-      <header className="flex justify-center items-center">
-        <p>{props.pageTitle}</p>
+      <header className="flex justify-center mask-bg flex-wrap">
+        <p className="delay-time04 flex justify-center items-center">
+          {props.pageTitle}
+        </p>
+        <div></div>
       </header>
     </div>
   );
--- components/information/myComponent.js
+++ components/information/myComponent.js
@@ -1,25 +1,39 @@
 /** @format */
 
-/** @format */
 import Image from "next/image";
 export default function MyComponent() {
   return (
     <div className="flex justify-center flex-wrap mx-2 my-4">
       <Image
-        className="w-full lg:w-1/2"
+        className="w-full lg:w-1/2 fadeUp"
         width={400}
         height={400}
         src="/img/illust/simplePersonPict.svg"
       />
       <div className="w-full lg:w-1/2 ">
-        <div className="h-1/4">
-          <p>VRChat楽しい</p>
+        <div className="h-1/4 fadeUp delay-time02 flex items-center py-2 justify-center">
+          <div className="w-1/12 ">
+            <span class="material-icons arrow-rotate">arrow_back</span>
+          </div>
+          <div className="w-11/12">
+            <p>VRChat楽しい</p>
+          </div>
         </div>
-        <div className="h-2/4">
-          <p>VRChat楽しい</p>
+        <div className="h-2/4 fadeUp delay-time02 flex items-center py-2 justify-center">
+          <div className="w-1/12 ">
+            <span class="material-icons arrow-rotate">arrow_back</span>
+          </div>
+          <div className="w-11/12">
+            <p>キャラメルポップコーン美味しい</p>
+          </div>
         </div>
-        <div className="h-1/4">
-          <p>VRChat楽しい</p>
+        <div className="h-2/4 fadeUp delay-time02 flex items-center py-2 justify-center">
+          <div className="w-1/12 ">
+            <span class="material-icons arrow-rotate">arrow_back</span>
+          </div>
+          <div className="w-11/12">
+            <p>VRChat楽しい</p>
+          </div>
         </div>
       </div>
     </div>
