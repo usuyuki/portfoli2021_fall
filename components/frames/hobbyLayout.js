e72cce76199dbead626cb8925148b65a4dd4ce19
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-01-21 21:02:33 +0900

    feat profileへの趣味追加

---
+++ components/frames/hobbyLayout.js
@@ -0,0 +1,28 @@
+/** @format */
+
+export default function HobbyLayout(props) {
+  console.log(props.content);
+  return (
+    <div className="flex  flex-wrap justify-center p-2  mx-4 lg:mx-80">
+      {props.content.map((value, key) => {
+        return (
+          <div
+            className="border-u_c_5 border-4 p-4 rounded-xl m-4 shadow-md md:w-1/3"
+            key={key}
+          >
+            <h3 className="font-semibold text-3xl text-center mb-1">
+              {value[0]}
+            </h3>
+            <p className="mx-4 mb-1 text-center">好き度:{value[1]}</p>
+            <div
+              className="mt-4 px-4 mb-2 h-auto mx-auto break-all"
+              dangerouslySetInnerHTML={{
+                __html: value[2],
+              }}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
