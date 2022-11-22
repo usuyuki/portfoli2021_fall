adf33eb7c0f52279512898b9e2c3976a5a18a8da
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-11-22 20:25:55 +0900

    feat keybase追加、bookmater廃止、微妙アニメーションの挙動を変更

--- components/information/ShortDescription.js
+++ components/information/ShortDescription.js
@@ -5,10 +5,10 @@ export default function ShortDescription() {
   return (
     <div className="flex justify-center flex-wrap mx-2 my-12">
       <Image
-        className="fadeUp"
+        className="fadeUp delay-time08"
         width={400}
         height={400}
-        src="/img/illust/acrylickeyholderUsuyuki.png"
+        src="/img/usuyuki/acrylickeyholderUsuyuki.png"
       />
       <div className="w-full flex flex-col justify-center">
         <div className="fadeUp delay-time02 flex items-center py-2 justify-center">
